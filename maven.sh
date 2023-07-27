3d6f30d31511c5fa6d5e26d37b7f14c45ffbe617
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-07-27 18:21:49 +0200

    Move content to separate files
    This should help to update README.md on commit

--- maven.sh
+++ maven.sh
@@ -1,29 +1,3 @@
 confDir=$(configDir)
 file="$confDir/.mavenrc"
-cat >"$file" <<EOL
-# mci - mvn clean install
-alias mci='mvn clean install'
-
-# mcv - mvn clean verify
-alias mcv='mvn clean verify'
-
-# mcp - mvn clean package
-alias mcp='mvn clean package'
-
-# mctc - mvn clean test-compile
-alias mctc='mvn clean test-compile'
-
-# mct - mvn clean test
-alias mct='mvn clean test'
-
-# mcist - mvn clean install -DskipTests
-alias mcist='mvn clean install -DskipTests'
-
-# mcpst - mvn clean package -DskipTests
-alias mcpst='mvn clean package -DskipTests'
-
-# mw - mvn wrapper:wrapper
-alias mw='mvn wrapper:wrapper'
-
-EOL
-
+cat ./.mavenrc >"$file"

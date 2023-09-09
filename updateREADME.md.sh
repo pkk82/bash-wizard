7608d5d976df7b4c4f807f911e9bd68877c251b7
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-09-09 10:54:56 +0200

    Exclude `.functionsrc` from `updateREADME.md.sh` script

--- updateREADME.md.sh
+++ updateREADME.md.sh
@@ -4,6 +4,7 @@ echo "" >> README.md
 for f in ./.*rc; do
   [[ -e "$f" ]] || break
   fileName=$(basename "$f")
+  [[ "$fileName" != ".functionsrc" ]] || continue
   echo "### ${fileName}" | sed 's/\.//g' | sed 's/rc//g' | tr '[:lower:]' '[:upper:]' >> README.md
   grep "#" <"$f" | sed 's/#//g' | sed 's/\(.*\) - \(.*\)/    # \2\n   \1\n/g' >> README.md
 done

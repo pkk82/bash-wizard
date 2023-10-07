c9ab5d6cfcbb3640c8fb3ac639a51232f7f351ce
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-10-07 11:17:25 +0200

    Make script compatible with `zsh`

--- updateREADME.md.sh
+++ updateREADME.md.sh
@@ -2,9 +2,9 @@ sed -i "/## Aliases & Functions/,\$d" README.md
 echo "## Aliases & Functions" >> README.md
 echo "" >> README.md
 for f in ./.*rc; do
-  [[ -e "$f" ]] || break
+  [ -e "$f" ] || break
   fileName=$(basename "$f")
-  [[ "$fileName" != ".functionsrc" ]] || continue
+  [ "$fileName" != ".functionsrc" ] || continue
   echo "### ${fileName}" | sed 's/\.//g' | sed 's/rc//g' | tr '[:lower:]' '[:upper:]' >> README.md
   grep "#" <"$f" | sed 's/#//g' | sed 's/\(.*\) - \(.*\)/    # \2\n   \1\n/g' >> README.md
 done
